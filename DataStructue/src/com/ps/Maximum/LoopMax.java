591f3010af8adfa53c6d981b31904451962ae3a7
panshuang <1404607506@qq.com>
2018-08-05 22:28:11 +0800

    idea配置新增以及最大子序列求解最坏算法

---
+++ DataStructue/src/com/ps/Maximum/LoopMax.java
@@ -0,0 +1,32 @@
+package com.ps.Maximum;
+
+/**
+ * @Author:panshuang
+ * @Data:2018/8/5 17:40
+ * @Description:三层循环求最大子序列
+ */
+public class LoopMax {
+    public  static int maximum(int[] arr) {
+        /**
+         * 最大值，初始为0
+         */
+        int maxnum = 0;
+        for (int i = 0; i <  arr.length; i++) {
+            for (int j=i; j < arr.length; j++) {
+                int thisSum = 0;
+                for (int k=i; k < j; k++) {
+                    thisSum += arr[k];
+                    if (thisSum > maxnum) {
+                        maxnum = thisSum;
+                    }
+                }
+            }
+        }
+        return maxnum;
+    }
+
+    public static void main (String[] args) {
+        int[] arr = new int[]{1,-2,3,5,-6,9,10,-6,-7,9};
+        System.out.println(maximum(arr));
+    }
+}

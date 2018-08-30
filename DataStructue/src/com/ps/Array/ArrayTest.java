0c52c1d30048da3af2135d2d4e5ce70d4ad91bb8
panshuang <1404607506@qq.com>
2018-08-30 23:56:10 +0800

    List以及Map的迭代器测试

--- DataStructue/src/com/ps/Array/ArrayTest.java
+++ DataStructue/src/com/ps/Array/ArrayTest.java
@@ -1,5 +1,7 @@
 package com.ps.Array;
 
+import java.util.*;
+
 /**
  * @Author:panshuang
  * @Data:2018/8/28 22:14
@@ -36,5 +38,29 @@ public class ArrayTest {
         //根据索引找寻测试
         System.out.println(arrInt.find(1));
 
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        //迭代器自带remove方法删除元素不会破坏迭代器的合法性
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()){
+            iterator.remove();
+        }
+        System.out.println(list.size());
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < 10; i++) {
+           map.put(i, i);
+        }
+
+        //mao需要转化为Set才能使用迭代器，且也只能进行remove操作，不能进行添加操作
+        Set<Map.Entry<Integer, Integer>> mapEntry = map.entrySet();
+        Iterator mapIterator = mapEntry.iterator();
+        while (mapIterator.hasNext()){
+            mapIterator.remove();
+        }
     }
 }

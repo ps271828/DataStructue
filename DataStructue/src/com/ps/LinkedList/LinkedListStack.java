f06f2288438e413a701e453e3c1b334949a44ee1
panshuang <1404607506@qq.com>
2018-09-11 00:07:22 +0800

    结合栈定义使用链表实现，后续根据视频改进

---
+++ DataStructue/src/com/ps/LinkedList/LinkedListStack.java
@@ -0,0 +1,79 @@
+package com.ps.LinkedList;
+
+import com.ps.stack.ArrayStack;
+
+/**
+ * @Author:panshuang
+ * @Data:2018/9/10 23:49
+ * @Description:¡¥±Ì µœ÷’ª
+ */
+public class LinkedListStack <T> implements ArrayStack <T> {
+
+    private class Node{
+        public T t;
+        public Node next;
+
+        public Node(T t, Node next){
+            this.t = t;
+            this.next = next;
+        }
+
+        public  Node(T t){
+            this(t, null);
+        }
+
+        public Node(){
+            this(null, null);
+        }
+    }
+
+    private Node top;
+    private Integer size;
+
+    public LinkedListStack(){
+        this.top = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void push(T t) {
+        if (this.top == null) {
+            this.top = new Node(t);
+        }else {
+          /*  Node curr = new Node(t);
+            curr.next = this.top;
+            this.top = curr;*/
+          this.top = new Node(t, this.top);
+        }
+        this.size ++;
+    }
+
+    @Override
+    public T pop() {
+        if (this.size == 0) {
+            throw new IllegalStateException("The stack is Empty!");
+        }
+        Node retNode = this.top;
+        this.top = this.top.next;
+        retNode.next = null;
+        this.size --;
+        return retNode.t;
+    }
+
+    @Override
+    public T peek() {
+        if (this.size == 0)
+            throw new IllegalStateException("The stack is Empty!@");
+        return this.top.t;
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+}
